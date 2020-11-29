7cbed4f837c8cbd8932658c1352eda18c91e9eb7
LAPTOP-1U1SBC38\ferna <ferbert15@gmail.com>
2020-11-29 18:31:51 +0100

    *add some style to the signin form

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -19,11 +19,11 @@ export class SignInComponent implements OnInit {
 
   formInputs(): void {
     this.FormSignIn = this.formConstructor.group({
-      name: ['', Validators.required],
-      familyName: ['', Validators.required],
+      name: ['', [Validators.required,Validators.minLength(3)]],
+      familyName: ['', [Validators.required,Validators.minLength(2)]],
       bornData: ['', Validators.required],
       city: ['', Validators.required],
-      email: ['', Validators.required],
+      email: ['',[ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
       phone: ['', Validators.required]
     })
   }
@@ -41,6 +41,30 @@ export class SignInComponent implements OnInit {
   } 
   get validFamilyName():boolean {
     return this.FormSignIn.get('familyName').valid && this.FormSignIn.get('familyName').touched;
+  }
+  get invalidData():boolean {
+    return this.FormSignIn.get('bornData').invalid && this.FormSignIn.get('bornData').touched;
+  } 
+  get validData():boolean {
+    return this.FormSignIn.get('bornData').valid && this.FormSignIn.get('bornData').touched;
+  }
+  get invalidCity():boolean {
+    return this.FormSignIn.get('city').invalid && this.FormSignIn.get('city').touched;
+  } 
+  get validCity():boolean {
+    return this.FormSignIn.get('city').valid && this.FormSignIn.get('city').touched;
+  }
+  get invalidEmail():boolean {
+    return this.FormSignIn.get('email').invalid && this.FormSignIn.get('email').touched;
+  } 
+  get validEmail():boolean {
+    return this.FormSignIn.get('email').valid && this.FormSignIn.get('email').touched;
+  }
+  get invalidPhone():boolean {
+    return this.FormSignIn.get('phone').invalid && this.FormSignIn.get('phone').touched;
+  } 
+  get validPhone():boolean {
+    return this.FormSignIn.get('phone').valid && this.FormSignIn.get('phone').touched;
   }
 
 
