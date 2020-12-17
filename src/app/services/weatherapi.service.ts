382a37ab40e7cc9b8efa7fad8e9819941eef20cc
DESKTOP-I06KB15\Jdom0 <josee94@hotmail.es>
2020-12-17 17:45:20 +0100

    actualizado estilos

--- src/app/services/weatherapi.service.ts
+++ src/app/services/weatherapi.service.ts
@@ -9,7 +9,8 @@ export class WeatherapiService {
 
   constructor(private http: HttpClient) { }
 
-  key: string;
+  key: string = "a460c7f006e7bcde381082ee230ef0bc"
+
 
   getWeather(city)  {
     return this.http.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.key}&units=metric&lang=sp`)  
